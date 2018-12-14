c42b101e000aa95a5dd110c0d1460044e8bf9bf6
JunyongPark97 <pjyong68@naver.com>
2018-12-14 21:42:29 +0900

    cc

--- user/admin.py
+++ user/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from user.models import User, Timelog, UpdateRequest
+from user.models import User, Timelog, UpdateRequest, EnterTimelog
 
 admin.site.register(User)
 # admin.site.register(Timelog)
 admin.site.register(UpdateRequest)
+admin.site.register(EnterTimelog)
