6eb32e883ca8d5cbe49199ea4714a5c4dc37c332
JunyongPark97 <pjyong68@naver.com>
2018-12-16 00:49:48 +0900

    test

--- user/admin.py
+++ user/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from user.models import User, Timelog, UpdateRequest, EnterTimelog, OutTimelog
+from user.models import User, Timelog, UpdateRequest, EnterTimelog, OutTimelog, EnterAtHomeTimelog, OutAtHomeTimelog
 
 admin.site.register(User)
 # admin.site.register(Timelog)
 admin.site.register(UpdateRequest)
 admin.site.register(EnterTimelog)
 admin.site.register(OutTimelog)
+admin.site.register(EnterAtHomeTimelog)
+admin.site.register(OutAtHomeTimelog)
